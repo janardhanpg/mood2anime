b098ee9bc97bf0926cc2063abca64bdee5d6dcf7
Janardhan <janardhanpg12@gmail.com>
2024-11-29 14:39:20 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import ContentContainer from "./components/ContentContainer";
+
+export default function App() {
+  return (
+    <>
+      
+      <ContentContainer />
+    </>
+  );
+}
---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+export default function Footer() {
+  return (
+    <>
+      <footer className="footer footer-center bg-base-100 text-base-content p-4">
+        <aside>
+          <p>
+            Made by &nbsp;
+            <a
+              target="_blank"
+              href="https://github.com/janardhanpg"
+              className="text-primary underline"
+            >
+              Janardhan
+            </a>
+          </p>
+        </aside>
+      </footer>
+    </>
+  );
+}
